d9b2c5bbd6cf7b8b343d688236bab3f9b58a4419
Martin Facorro <mfacorro@live.com.ar>
2023-02-05 19:38:35 -0300

    Add a new Component

---
+++ 03-COUNTER-APP-VITE/counter-app/src/FirstApp.jsx
@@ -0,0 +1,6 @@
+export function FirstApp() {
+  return (
+    <h2>This is the FirstApp Component</h2>
+  );
+}
+
--- 03-COUNTER-APP-VITE/counter-app/src/main.jsx
+++ 03-COUNTER-APP-VITE/counter-app/src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client"; //HERRAMIENTA PARA RENDERIZAR
 
 import {App} from './HelloWorldApp'
+import {FirstApp} from './FirstApp';
+
 
 //RENDERIZARLA EN EL DOM
 ReactDOM.createRoot( document.getElementById('root') ).render(
   <React.StrictMode>
     <App />
+    <FirstApp />
   </React.StrictMode>
 );
