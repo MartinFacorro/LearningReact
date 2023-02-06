27aab03638d71abedc7bd452112d6ad009e7799c
Martin Facorro <mfacorro@live.com.ar>
2023-02-06 20:39:37 -0300

    Impresión de variables en el HTML

--- 03-COUNTER-APP-VITE/counter-app/src/FirstApp.jsx
+++ 03-COUNTER-APP-VITE/counter-app/src/FirstApp.jsx
@@ -1,30 +1,14 @@
-//Forma 1 Para que haya un elemento padre
-/*export function FirstApp() {
+const newMessage = {
+  message : 'Hola mundo',
+  title : 'Hola React'
+};
+
+export const FirstApp = () => {
   return (
-    <div>
-      <h2>This is the FirstApp Component</h2>
-      <h3>Pruebas de otro elemento</h3>
-    </div>
-  );
-}
-
-// Forma dos, se importa el elemento fragment
-
-export function FirstApp() {
-  return (
-    <fragmemt>
-      <h2>This is the FirstApp Component</h2>
-      <h3>Pruebas de otro elemento</h3>
-    </fragmemt>
-  );
-}*/
-
-
-export function FirstApp() {
-  return (
-    <> // SINONIMO DE FRAGMENTO
-      <h2>This is the FirstApp Component</h2>
-      <h3>Pruebas de otro elemento</h3>
+    <>
+    <h1>
+      <code>{ JSON.stringify(newMessage) }</code>
+      </h1>
     </>
-  );
+  )
 }
