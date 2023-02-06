e7040c5d7c7e75e88b4ea36e32e108ac1400c16b
Martin Facorro <mfacorro@live.com.ar>
2023-02-06 07:36:28 -0300

    3 uses of fragment

--- 03-COUNTER-APP-VITE/counter-app/src/FirstApp.jsx
+++ 03-COUNTER-APP-VITE/counter-app/src/FirstApp.jsx
@@ -1,6 +1,30 @@
-export function FirstApp() {
+//Forma 1 Para que haya un elemento padre
+/*export function FirstApp() {
   return (
-    <h2>This is the FirstApp Component</h2>
+    <div>
+      <h2>This is the FirstApp Component</h2>
+      <h3>Pruebas de otro elemento</h3>
+    </div>
   );
 }
 
+// Forma dos, se importa el elemento fragment
+
+export function FirstApp() {
+  return (
+    <fragmemt>
+      <h2>This is the FirstApp Component</h2>
+      <h3>Pruebas de otro elemento</h3>
+    </fragmemt>
+  );
+}*/
+
+
+export function FirstApp() {
+  return (
+    <> // SINONIMO DE FRAGMENTO
+      <h2>This is the FirstApp Component</h2>
+      <h3>Pruebas de otro elemento</h3>
+    </>
+  );
+}
