1028413b34c13a228eb6ab37506b203f6ea3c82a
Martin Facorro <mfacorro@live.com.ar>
2023-02-06 21:11:28 -0300

    Add PropTypes package & test

--- 03-COUNTER-APP-VITE/counter-app/src/FirstApp.jsx
+++ 03-COUNTER-APP-VITE/counter-app/src/FirstApp.jsx
@@ -1,10 +1,17 @@
-export const FirstApp = ({title, subtitle}) => {
+import PropTypes from "prop-types";
+
+export const FirstApp = ({ title, subtitle }) => {
 
 
   return (
     <>
-    <h1>{title}</h1>
-    <p>{subtitle}</p>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
     </>
-  )
+  );
+};
+
+
+FirstApp.prototype = {
+  title: PropTypes.string.isRequired // DEBE SER string
 }
--- 03-COUNTER-APP-VITE/counter-app/src/main.jsx
+++ 03-COUNTER-APP-VITE/counter-app/src/main.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; //HERRAMIENTA PARA RENDERIZAR
 
-import {App} from './HelloWorldApp'
-import {FirstApp} from './FirstApp';
+import { App } from "./HelloWorldApp";
+import { FirstApp } from "./FirstApp";
 
-import './styles.css' //ESTILO GLOBAL
-
+import "./styles.css"; //ESTILO GLOBAL
 
 //RENDERIZARLA EN EL DOM
-ReactDOM.createRoot( document.getElementById('root') ).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <FirstApp title="Hola Soy Batman" subtitle={123} />
+    <FirstApp title={123}/>
   </React.StrictMode>
 );
